1497b78ee2683376b7010dc6aa02a0ef225ed36e
Tomas Zingis <thebash2344@gmail.com>
2025-02-06 19:58:55 +0100

    done

---
+++ js/task-2.js
@@ -0,0 +1,15 @@
+function makeArray(firstArray, secondArray, maxLength) {
+  let finalarray = firstArray.concat(secondArray);
+  if (finalarray.length > maxLength) {
+    return (finalarray = finalarray.slice(0, maxLength));
+  } else {
+    return finalarray;
+  }
+}
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
